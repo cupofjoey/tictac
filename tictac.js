bb0e573b46925c5a6e0c9e0529fcad9206a40069
Joe Hill <joe@jkh3.com>
2016-05-24 21:39:49 -0400

    Almost Done. Have conditions for victory

--- tictac.js
+++ tictac.js
@@ -7,13 +7,6 @@ Square.prototype.isOccupied = function() {
 	return this.marker ? true : false;
 };
 
-var board = [];
-
-for (var i = 1; i < 10; i++) {
-	board.push(new Square(i));
-}
-
-	console.log(board);
 var Player = function(marker) {
 	this.marker = marker;
 	this.score = 0;
@@ -43,6 +36,38 @@ AI.prototype.executeMove = function(){
 		var randomPlace = Math.floor(Math.random() * (9));
 		stopFlag = this.placeMarker(board[randomPlace]);
 	}
-
-
 };
+
+var Game = function() {
+	this.winningMoves = [
+		[1,2,3], [4,5,6], [7,8.9], 
+		[1,4,7], [2,5,8], [3,6,9], 
+		[1,5,9], [3,5,7]
+	];
+	this.createBoard();
+};
+
+Game.prototype.createBoard = function(){
+	this.board = [];
+
+	for (var i = 1; i < 10; i++) {
+		this.board.push(new Square(i));
+	}
+
+	console.log(this.board);
+};
+
+Game.prototype.awardVictory = function(player) {
+	player.score++
+};
+
+Game.prototype.checkVictory = function(){
+	var(var i = 0; i < this.winningMoves.length; i++) {
+		var candidateArray = this.winningMoves[i];
+		if(candidateArray[0].marker == candidateArray[1].marker 
+		&& candidateArray[0].marker == candidateArray[2].marker) {
+			return true;
+		}
+	}
+}
+
