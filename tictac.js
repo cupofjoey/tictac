e3b90daeb325d5691cc0da0810126174dfeac5b6
Joe Hill <joe@jkh3.com>
2016-05-24 20:56:14 -0400

    AI Randomized

--- tictac.js
+++ tictac.js
@@ -12,17 +12,30 @@ var board = [];
 for (var i = 1; i < 10; i++) {
 	board.push(new Square(i));
 }
-
+	console.log(board);
 var Player = function(marker) {
 	this.marker = marker;
 	this.score = 0;
 };
 
 Player.prototype.placeMarker = function(square) {
-	if(square.isOccupied) {
+	if(square.isOccupied()) {
+		console.log("space occupied for square " + square.place);
 
 	} else {
 		square.marker = this.marker;
-		
+		console.log("success");
 	}
+};
+
+var AI = function(marker) {
+	Player.call(this, marker);
+};
+
+AI.prototype = Object.create(Player.prototype);
+AI.prototype.constructor = AI;
+
+AI.prototype.executeMove = function(){
+	var randomPlace = Math.floor(Math.random() * (9));
+	this.placeMarker(board[randomPlace]);
 };
