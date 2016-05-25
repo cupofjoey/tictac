b57bec9708b1d2ed0e1c41b657b73d34ceae1569
Joe Hill <joe@jkh3.com>
2016-05-24 20:02:45 -0400

    instantiate 9 squares

--- tictac.js
+++ tictac.js
@@ -1,1 +1,15 @@
-alert("test");
+var Square = function (place) {
+	this.place = place;
+	this.occupied = null;
+};
+
+Square.prototype.isOccupied = function() {
+	return this.occupied ? true : false;
+};
+
+var board = [];
+
+for (var i = 1; i < 10; i++) {
+	board.push(new Square(i));
+}
+console.log(board);
