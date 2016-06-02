996fb13dacc4e1b4cb3be1f63b5f2397f331ce51
Joe Hill <joe@jkh3.com>
2016-06-01 20:35:58 -0400

    Cleaning up AI and Create Board Functions

--- tictac.js
+++ tictac.js
@@ -7,6 +7,16 @@ Square.prototype.isOccupied = function() {
 	return this.marker ? true : false;
 };
 
+Square.prototype.generateElement = function(gameInstance) {
+	var squareScope = this;
+	var squareElement = document.createElement("div");
+	squareElement.setAttribute("class", "square");
+	squareElement.addEventListener("click", function(){
+		gameInstance.commenceTurn(squareScope);
+	});
+	gameInstance.boardElement.appendChild(squareElement);
+};
+
 var Player = function(marker) {
 	this.marker = marker;
 	this.score = 0;
@@ -25,7 +35,7 @@ Player.prototype.placeMarker = function(square) {
 };
 
 Player.prototype.executeMove = function(square){
-	this.placeMarker(square);
+	return this.placeMarker(square);
 }
 
 var AI = function(marker) {
@@ -39,9 +49,9 @@ AI.prototype.executeMove = function(){
 	var stopFlag = false;
 	while(!stopFlag){
 		var randomPlace = Math.floor(Math.random() * (9));
-		game.board;
-		stopFlag = this.placeMarker(board[randomPlace]); //TODO Decouple AI's dependence on Game.board creation
+		stopFlag = this.placeMarker(game.board[randomPlace]); //TODO Decouple AI's dependence on Game.board creation
 	}
+	return stopFlag;
 };
 
 var Game = function() {
@@ -56,10 +66,15 @@ var Game = function() {
 
 Game.prototype.createBoard = function(){
 	this.board = [];
-	for(var i = 1; i < 10; i++) {
-		this.board.push(new Square(i));
-	}
+	this.boardElement = document.createElement("div");
+	this.boardElement.setAttribute("id", "board");
 
+	for(var i = 1; i < 10; i++) {
+		var square = new Square(i);
+		square.generateElement(this);
+		this.board.push(square);
+	}
+	document.body.appendChild(this.boardElement);
 	console.log(this.board);
 };
 
@@ -68,7 +83,7 @@ Game.prototype.awardVictory = function(player) {
 };
 
 Game.prototype.checkVictory = function(){
-	for(var i = 0; i < this.winningMoves.length; i++) {
+	for(var i = 0; i < this.winningMoves.length; i++) { //need to double check our array assignments here
 		var candidateArray = this.winningMoves[i];
 		var place1 = candidateArray[0];
 		var place2 = candidateArray[1];
@@ -84,15 +99,16 @@ Game.prototype.checkVictory = function(){
 }
 
 Game.prototype.commenceTurn = function(square){
-	this.currentPlayer.executeMove(square);
-	if(this.checkVictory()) {
-		this.awardVictory(this.currentPlayer);
-		this.createBoard();
-		this.currentPlayer = human;
-	}else{
-		this.currentPlayer = this.currentPlayer == human ? ai : human;
-		if(this.currentPlayer == ai){
-			this.commenceTurn();
+	if (this.currentPlayer.executeMove(square)) {
+		if(this.checkVictory()) {
+			this.awardVictory(this.currentPlayer);
+			this.createBoard();
+			this.currentPlayer = human;
+		}else{
+			this.currentPlayer = this.currentPlayer == human ? ai : human;
+			if(this.currentPlayer == ai){
+				this.commenceTurn();
+			}
 		}
 	}
 };
